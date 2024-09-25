91f73a8539c017af522318af7032052e061aced5
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-09-26 04:53:25 +0700

    Update infinit.sh

--- infinit/infinit.sh
+++ infinit/infinit.sh
@@ -1,49 +1,41 @@
 #!/bin/bash
 
-
-print_colored() {
-    echo -e "\e[$1m$2\e[0m"
+function show {
+  echo -e "\e[1;34m$1\e[0m"
 }
-
 
 display_colored_text() {
     print_colored "42;30" "========================================================="
     print_colored "46;30" "========================================================="
-    print_colored "45;97" "======================  Infinit ========================="
+    print_colored "45;97" "======================   T3EN   ========================="
     print_colored "43;30" "============== create all by JAWA-PRIDE  ================"
     print_colored "41;97" "=========== https://t.me/AirdropJP_JawaPride ============"
     print_colored "44;30" "========================================================="
     print_colored "42;97" "========================================================="
 }
 
-
 display_colored_text
 sleep 5
 
-
-function show {
-  echo -e "\e[1;34m$1\e[0m"
-}
-
 export NVM_DIR="$HOME/.nvm"
 if [ -s "$NVM_DIR/nvm.sh" ]; then
-    show "Memuat NVM..."
+    show "Loading NVM..."
     echo
     source "$NVM_DIR/nvm.sh"
 else
-    show "NVM tidak ditemukan, menginstal NVM..."
+    show "NVM not found, installing NVM..."
     echo
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
     source "$NVM_DIR/nvm.sh"
 fi
 
 echo
-show "Menginstal Node.js..."
+show "Installing Node.js..."
 echo
 nvm install 22 && nvm alias default 22 && nvm use default
 echo
 
-show "Menginstal Foundry..."
+show "Installing Foundry..."
 echo
 curl -L https://foundry.paradigm.xyz | bash
 export PATH="$HOME/.foundry/bin:$PATH"
@@ -51,7 +43,7 @@ sleep 5
 source ~/.bashrc
 foundryup
 
-show "Menginstal Bun..."
+show "Installing Bun..."
 echo
 curl -fsSL https://bun.sh/install | bash
 export PATH="$HOME/.bun/bin:$PATH"
@@ -59,31 +51,42 @@ sleep 5
 source ~/.bashrc
 echo
 
-show "Menyiapkan proyek Bun..."
+show "Setting up Bun project..."
 echo
-mkdir infinit && cd infinit
+mkdir -p ~/infinit && cd ~/infinit
 bun init -y
 bun add @infinit-xyz/cli
 echo
 
+show "Creating infinit.config.yaml..."
+echo
+cat <<EOF > infinit.config.yaml
+network:
+  name: holesky
+  url: https://endpoints.omniatech.io/v1/eth/holesky/public
+EOF
+echo "File infinit.config.yaml berhasil dibuat."
+
+show "Mengimpor wallet..."
+echo
+read -p "Masukkan private key wallet Anda: " PRIVATE_KEY
+
 show "Inisialisasi Infinit CLI dan menghasilkan akun..."
 echo
-bunx infinit init
-bunx infinit account generate
+bunx infinit account import --private-key "$PRIVATE_KEY"
+
+read -p "Wallet address (masukkan address dari step sebelumnya) : " WALLET
+echo
+read -p "account ID (lakukan seperti diatas) : " ACCOUNT_ID
 echo
 
-read -p "Alamat dompet (masukkan address dari step sebelumnya) : " WALLET
-echo
-read -p "ID akun (lakukan seperti diatas) : " ACCOUNT_ID
-echo
-
-show "Salin kunci pribadi dan simpan"
+show "Copy private key dan simpan "
 echo
 bunx infinit account export $ACCOUNT_ID
 
 sleep 5
 echo
-
+# Removing old deployUniswapV3Action script if exists
 rm -rf src/scripts/deployUniswapV3Action.script.ts
 
 cat <<EOF > src/scripts/deployUniswapV3Action.script.ts
@@ -92,13 +95,22 @@ import type { z } from 'zod'
 
 type Param = z.infer<typeof actions['init']['paramsSchema']>
 
+// TODO: Replace with actual params
 const params: Param = {
+  // Native currency label (e.g., ETH)
   "nativeCurrencyLabel": 'ETH',
+
+  // Address of the owner of the proxy admin
   "proxyAdminOwner": '$WALLET',
+
+  // Address of the owner of factory
   "factoryOwner": '$WALLET',
+
+  // Address of the wrapped native token (e.g., WETH)
   "wrappedNativeToken": '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 }
 
+// Signer configuration
 const signer = {
   "deployer": "$ACCOUNT_ID"
 }
@@ -106,6 +118,6 @@ const signer = {
 export default { params, signer, Action: DeployUniswapV3Action }
 EOF
 
-show "Menjalankan skrip UniswapV3..."
+show "Executing the UniswapV3 Action script..."
 echo
 bunx infinit script execute deployUniswapV3Action.script.ts
