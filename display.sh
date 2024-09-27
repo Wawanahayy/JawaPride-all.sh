62e229c46350ef02d8b8881b11e3683bb8f95d1d
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-09-27 10:29:53 +0700

    Update display.sh

--- display.sh
+++ display.sh
@@ -24,14 +24,21 @@ log() {
     echo "[$level] $message"
 }
 
+# Pertanyaan untuk bergabung dengan channel
 while true; do
     read -p "Apakah Anda sudah bergabung dengan channel kami Channel: @AirdropJP_JawaPride? (y/n): " join_channel
-    if [[ "$join_channel" == "y" || "$join_channel" == "Y" ]]; then
-        break
-    elif [[ "$join_channel" == "n" || "$join_channel" == "N" ]]; then
-        echo "Silakan bergabung dengan channel terlebih dahulu."
-        exit 1
-    else
-        echo "Pilihan tidak valid. Harap masukkan 'y' atau 'n'."
-    fi
+    echo "Input: $join_channel"  # Menampilkan input untuk debug
+    case "$join_channel" in
+        [yY]* ) 
+            echo "Terima kasih telah bergabung!"
+            break 
+            ;;
+        [nN]* ) 
+            echo "Silakan bergabung dengan channel terlebih dahulu." && exit 1 
+            ;;
+        * ) 
+            echo "Pilihan tidak valid. Harap masukkan 'y' atau 'n'." 
+            ;;
+    esac
 done
+
