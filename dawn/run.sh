9b9ffd47e35de5cba850e0e2bb1609d0f64f187e
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-09-27 13:34:38 +0700

    Update run.sh

--- dawn/run.sh
+++ dawn/run.sh
@@ -55,7 +55,7 @@ function install_node() {
 
     echo $DAWNUSERNAME:$DAWNPASSWORD > password.txt
 
-    wget -O dawn.py https://raw.githubusercontent.com/b1n4he/DawnAuto/main/dawn.py
+    wget -O dawn.py https://raw.githubusercontent.com/Wawanahayy/JawaPride-all.sh/refs/heads/main/dawn/dawn.py
     # Memperbarui dan menginstal perangkat lunak yang diperlukan
     sudo apt update && sudo apt upgrade -y
     sudo apt install -y curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev lz4 snapd
