45fe46a4db61b2e70d4b270a868cabc364b917d4
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2025-04-13 20:38:40 +0700

    Update ritual.sh

--- ritual.sh
+++ ritual.sh
@@ -193,9 +193,9 @@ EOL
     # Menjalankan docker-compose setelah membuat service systemd
     echo "Menjalankan Docker Compose secara langsung..."
     cd /root/infernet-container-starter
+ docker-compose -f deploy/docker-compose.yaml up -d
  docker-compose down
  docker-compose up --build
- docker-compose -f deploy/docker-compose.yaml up -d
     echo "Docker Compose dijalankan."
     echo "Node Ritual berhasil dipasang dan dijalankan via docker"
 }
