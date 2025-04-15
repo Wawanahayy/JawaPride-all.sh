61900a6f2686ab2bb8ef9794c143a92f10da4f6a
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2025-04-15 21:45:09 +0700

    Update Drosera.sh

--- Drosera.sh
+++ Drosera.sh
@@ -5,32 +5,37 @@ sleep 3
 
 echo "Masukkan PRIVATE KEY Anda:"
 read PRIVATE_KEY
+
+echo "Masukkan IP VPS Anda:"
+read VPS_IP
+
+echo "Masukkan Email GitHub Anda:"
+read GITHUB_EMAIL
+
+echo "Masukkan Username GitHub Anda:"
+read GITHUB_USERNAME
+
 if [ -z "$PRIVATE_KEY" ]; then
     echo "Private Key tidak boleh kosong!"
     exit 1
 fi
 
-echo "Masukkan IP VPS Anda:"
-read VPS_IP
 if [ -z "$VPS_IP" ]; then
     echo "IP VPS tidak boleh kosong!"
     exit 1
 fi
 
-echo "Masukkan Email GitHub Anda:"
-read GITHUB_EMAIL
 if [ -z "$GITHUB_EMAIL" ]; then
     echo "Email GitHub tidak boleh kosong!"
     exit 1
 fi
 
-echo "Masukkan Username GitHub Anda:"
-read GITHUB_USERNAME
 if [ -z "$GITHUB_USERNAME" ]; then
     echo "Username GitHub tidak boleh kosong!"
     exit 1
 fi
 
+sleep 5
 sudo apt-get install python3 python3-pip -y > /dev/null 2>&1
 pip3 install eth-account > /dev/null 2>&1
 
